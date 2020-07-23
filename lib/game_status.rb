dc7c3dd0d571356ac5e9b2f984a9f742065551f5
Marques Avery <averymarques4@gmail.com>
2020-07-23 20:19:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,10 +54,9 @@ def over?(board)
       true
     elsif won?(board) == w
       true
-    else
-      false
     end
   end
+  false
 #  if draw?(board) == true
 #    true
 #  elsif WIN_COMBINATIONS.all?{|w| won?(board) == w }
