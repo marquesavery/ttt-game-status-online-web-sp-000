e1a06cf61c37dd2158dd5e5e2e3e524c92859673
Marques Avery <averymarques4@gmail.com>
2020-07-23 19:04:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,8 +27,10 @@ def won?(board)
     position_3 = board[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
+      true
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
+      true
     end
   end
   false
